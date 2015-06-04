a65b14dcb3dc716a372d4d995412e71bdeed8510
Dagmara Kotecka <dagkotecka@gmail.com>
2015-06-04 21:02:37 +0200

    extern C part 2

--- gol.cu
+++ gol.cu
@@ -67,55 +67,58 @@ calculate_new_status(const unsigned int *board, unsigned int *new_board, const i
 		int jj = aa % SIZE;
 		int alive_neighbours = 0;
 
-		alive_neighbours += (valid(ii - 1, jj - 1) ? board[XY(ii - 1, jj - 1)] : 0) ? 1 : 0;
-		alive_neighbours += (valid(ii - 1, jj) ? board[XY(ii - 1, jj)] : 0) ? 1 : 0;
-		alive_neighbours += (valid(ii - 1, jj + 1) ? board[XY(ii - 1, jj + 1)] : 0) ? 1 : 0;
-		alive_neighbours += (valid(ii, jj - 1) ? board[XY(ii, jj - 1)] : 0) ? 1 : 0;
-		alive_neighbours += (valid(ii, jj + 1) ? board[XY(ii, jj + 1)] : 0) ? 1 : 0;
-		alive_neighbours += (valid(ii + 1, jj - 1) ? board[XY(ii + 1, jj - 1)] : 0) ? 1 : 0;
-		alive_neighbours += (valid(ii + 1, jj) ? board[XY(ii + 1, jj)] : 0) ? 1 : 0;
-		alive_neighbours += (valid(ii + 1, jj + 1) ? board[XY(ii + 1, jj + 1)] : 0) ? 1 : 0;
+		if ((ii != 0) || (ii != (SIZE - 1)))
+		{
+			alive_neighbours += (valid(ii - 1, jj - 1) ? board[XY(ii - 1, jj - 1)] : 0) ? 1 : 0;
+			alive_neighbours += (valid(ii - 1, jj) ? board[XY(ii - 1, jj)] : 0) ? 1 : 0;
+			alive_neighbours += (valid(ii - 1, jj + 1) ? board[XY(ii - 1, jj + 1)] : 0) ? 1 : 0;
+			alive_neighbours += (valid(ii, jj - 1) ? board[XY(ii, jj - 1)] : 0) ? 1 : 0;
+			alive_neighbours += (valid(ii, jj + 1) ? board[XY(ii, jj + 1)] : 0) ? 1 : 0;
+			alive_neighbours += (valid(ii + 1, jj - 1) ? board[XY(ii + 1, jj - 1)] : 0) ? 1 : 0;
+			alive_neighbours += (valid(ii + 1, jj) ? board[XY(ii + 1, jj)] : 0) ? 1 : 0;
+			alive_neighbours += (valid(ii + 1, jj + 1) ? board[XY(ii + 1, jj + 1)] : 0) ? 1 : 0;
 
 #if DEBUG
-		new_board[XY(ii, jj)] = alive_neighbours;
+			new_board[XY(ii, jj)] = alive_neighbours;
 #else
-		unsigned int tmp_cell_val = 0;
+			unsigned int tmp_cell_val = 0;
 
-		if (board[XY(ii, jj)] == 0)
-		{
-			if (alive_neighbours == 3)
+			if (board[XY(ii, jj)] == 0)
 			{
-				tmp_cell_val = 1;
+				if (alive_neighbours == 3)
+				{
+					tmp_cell_val = 1;
+				}
+				else
+				{
+					tmp_cell_val = 0;
+				}
 			}
 			else
 			{
-				tmp_cell_val = 0;
+				if (alive_neighbours < 2)
+				{
+					tmp_cell_val = 0;
+				}
+				else if (alive_neighbours > 3)
+				{
+					tmp_cell_val = 0;
+				}
+				else
+				{
+					tmp_cell_val = 1;
+				}
 			}
-		}
-		else
-		{
-			if (alive_neighbours < 2)
-			{
-				tmp_cell_val = 0;
-			}
-			else if (alive_neighbours > 3)
-			{
-				tmp_cell_val = 0;
-			}
-			else
-			{
-				tmp_cell_val = 1;
-			}
-		}
 
-		new_board[XY(ii, jj)] = tmp_cell_val;
+			new_board[XY(ii, jj)] = tmp_cell_val;
 #endif // DEBUG
+		}
 	}
 }
 
-extern "C" int* gameOfLife(unsigned int* cells, int size)
+extern "C" int* gameOfLife(int* cells, int size)
 {
-	if (argc < 3) exit(EXIT_FAILURE);
+	if (size < 3) exit(EXIT_FAILURE);
 	else SIZE = size;
 
 	int THREADS_PER_BLOCK = 16;
@@ -123,22 +126,16 @@ extern "C" int* gameOfLife(unsigned int* cells, int size)
 
 	cudaError_t error = cudaSuccess;
 	srand(time(NULL));
+	unsigned int *board = NULL;
+	board = (unsigned int*)malloc(SIZE * SIZE * sizeof(unsigned int*));
 
-	cells = (unsigned int*)malloc(SIZE * SIZE * sizeof(unsigned int*));
-
-	if (cells == NULL)
+	if (board== NULL)
 	{
 		fprintf(stderr, "Malloc failed!\n");
 		exit(EXIT_FAILURE);
 	}
 
-	for (uint ii = 0; ii < SIZE; ii++)
-	{
-		for (uint jj = 0; jj < SIZE; jj++)
-		{
-			cells[XY(ii, jj)] = (unsigned int)rand() % 2;
-		}
-	}
+	board = cells;
 
 	unsigned int *d_board = NULL;
 	unsigned int *d_new_board = NULL;
@@ -149,14 +146,14 @@ extern "C" int* gameOfLife(unsigned int* cells, int size)
 	error = cudaMalloc((void **)&d_new_board, SIZE*SIZE*sizeof(unsigned int));
 	check_error(error, e_malloc);
 
-	error = cudaMemcpy(d_board, cells, SIZE*SIZE*sizeof(unsigned int), cudaMemcpyHostToDevice);
+	error = cudaMemcpy(d_board, board, SIZE*SIZE*sizeof(unsigned int), cudaMemcpyHostToDevice);
 	check_error(error, e_memcpyHtD);
 
 	calculate_new_status << <BLOCKS_PER_GRID, THREADS_PER_BLOCK >> >(d_board, d_new_board, SIZE);
 	error = cudaGetLastError();
 	check_error(error, e_kernel);
 
-	error = cudaMemcpy(cells, d_new_board, SIZE*SIZE*sizeof(unsigned int), cudaMemcpyDeviceToHost);
+	error = cudaMemcpy(board, d_new_board, SIZE*SIZE*sizeof(unsigned int), cudaMemcpyDeviceToHost);
 	check_error(error, e_memcpyDtH);
 
 	error = cudaFree(d_board);
@@ -168,5 +165,5 @@ extern "C" int* gameOfLife(unsigned int* cells, int size)
 	error = cudaDeviceReset();
 	check_error(error, e_reset);
 
-	return cells;
+	return board;
 }
