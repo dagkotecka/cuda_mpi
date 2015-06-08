bb67b113e0eb282590f46238d44eada5149682ea
Dagmara Kotecka <dagkotecka@gmail.com>
2015-06-08 22:57:41 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+echo "building...";
+
+echo "cuda gol"
+nvcc -arch=sm_20 -c gol.cu -o gol.o
+echo "mpi"
+/opt/mpich/ch-p4/bin/mpicc -c mpi.c -o mpi.o -lm
+echo "mpi cuda"
+/opt/mpich/ch-p4/bin/mpicc mpi.o gol.o -o mpi_gol -L/usr/local/cuda/targets/x86_64-linux/lib -L/usr/lib64/ -lcudart -lstdc++
+rm gol.o mpi.o
